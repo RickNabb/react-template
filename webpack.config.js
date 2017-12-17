6c834bd1d8855334bc17ce5c430d42149eb048c9
Nick Rabb <nrabb@outlook.com>
2017-12-17 15:04:30 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+const APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+const config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  module: {
+    loaders: [ 
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
